ff314c8259e82f4faa7fa06e1eb64ce3bd132798
m1m0zzz <117814895+m1m0zzz@users.noreply.github.com>
2024-08-25 05:51:58 +0000

    add: og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,32 @@ import "./globals.css";
 
 const noteSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
+const title = "mmz_tools | Ableton Racks Pack"
+const description = "Ableton Live Racks mmz_tools"
+const siteUrl = "https://m1m0zzz.github.io/mmz_tools/"
+const imageUrl = siteUrl + "preview.png"
+
 export const metadata: Metadata = {
-  title: "mmz_tools",
-  description: "Ableton Live Racks mmz_tools",
+  title: title,
+  description: description,
+  keywords: ["Music", "DTM", "Ableton", "Live", "Plugin"],
+  openGraph: {
+    type: "website",
+    url: siteUrl,
+    title: title,
+    description: description,
+    siteName: "mmz_tools",
+    images: [{
+      url: imageUrl,
+    }],
+  },
+  twitter: {
+    title: title,
+    description: description,
+    card: "summary_large_image",
+    creator: "@mimoz",
+    images: imageUrl
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" prefix="og: http://ogp.me/ns#">
       <body className={noteSansJP.className}>{children}</body>
     </html>
   );
