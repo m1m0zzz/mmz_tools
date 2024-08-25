0ae3bde762fa44e1506ad99da863423a499f2ee0
m1m0zzz <117814895+m1m0zzz@users.noreply.github.com>
2024-08-25 06:30:09 +0000

    add: share button

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,3 +1,4 @@
+import { FiShare2 } from "react-icons/fi";
 import styles from "./index.module.css";
 
 interface Props {
@@ -9,20 +10,27 @@ export default function Nav({ lang, onUpdate }: Props) {
   return (
     <nav className={styles.nav}>
       <h1>mmz_tools</h1>
-      <div className={styles.lang}>
-        <button
-          className={styles.lang_button}
-          style={lang == "ja" ? {color: "blue"} : {}}
-          disabled={lang == "ja"}
-          onClick={() => onUpdate("ja")}
-        >JA</button>
-        <span> / </span>
-        <button
-          className={styles.lang_button}
-          style={lang == "en" ? {color: "blue"} : {}}
-          disabled={lang == "en"}
-          onClick={() => onUpdate("en")}
-        >EN</button>
+      <div className={styles.right}>
+        <a
+          className={styles.share_button}
+          href="http://twitter.com/share?url=https://m1m0zzz.github.io/mmz_tools&text=mmz_tools%20%7C%20Ableton%20Racks%20Pack">
+          <FiShare2 size={"1rem"} />
+        </a>
+        <div className={styles.lang}>
+          <button
+            className={styles.lang_button}
+            style={lang == "ja" ? {color: "blue"} : {}}
+            disabled={lang == "ja"}
+            onClick={() => onUpdate("ja")}
+          >JA</button>
+          <span> / </span>
+          <button
+            className={styles.lang_button}
+            style={lang == "en" ? {color: "blue"} : {}}
+            disabled={lang == "en"}
+            onClick={() => onUpdate("en")}
+          >EN</button>
+        </div>
       </div>
     </nav>
   )
